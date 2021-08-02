3d81280ac73ee31ce86dd058873a825590a4334d
Pavaresorn Malai <61514399+Varkaria@users.noreply.github.com>
2021-08-02 15:59:53 +0700

    man

--- pages/components/beatmap.vue
+++ pages/components/beatmap.vue
@@ -1,13 +1,17 @@
 <template>
-  <Beatmap />
+  <div>
+    <Beatmap />
+    <p class="mt-2 text-center">I don't have Torus font pls donate</p>
+    <NuxtLink to="/"><p class="mt-2 text-center">Home</p></NuxtLink>
+  </div>
 </template>
 
 <script>
-import Beatmap from '~/components/Beatmap.vue'
+import Beatmap from "~/components/Beatmap.vue";
 
 export default {
   components: {
-    Beatmap
-  }
-}
+    Beatmap,
+  },
+};
 </script>
