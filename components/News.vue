8b44e3ce8f6983bf50e8b365c03741c80a88c333
Kitpipat Jaritwong <Kitpipat.0647@gmail.com>
2021-08-02 22:40:05 +0700

    [+] layout for index page

--- components/News.vue
+++ components/News.vue
@@ -1,5 +1,25 @@
 <template>
-  <div>
-    
+  <div class="flex flex-col h-auto rounded-xl bg-[#2E3835]">
+    <div class="relative">
+      <div class="absolute left-0 bg-black bg-opacity-75 rounded-lg m-4 px-2 py-1">
+        <h2 class="text-white font-bold text-xs uppercase">osu!dev</h2>
+      </div>
+      <div class="absolute right-0 bg-black bg-opacity-75 rounded-lg m-4 px-2 py-1">
+        <h2 class="text-white font-bold text-xs uppercase">8 Aug 2021</h2>
+      </div>
+      <img src="~/assets/News.png" class="w-full">
+    </div>
+    <div class="flex flex-wrap py-2 px-3">
+      <h1 class="text-lg text-white font-semibold">osu!ApexLegend</h1>
+      <p class="text-sm text-white">
+        We are happy to announce the release of osu!ApexLegend, a new modpack for osu! that is based on the original osu! stable.
+      </p>
+    </div>
   </div>
 </template>
+
+<style>
+* {
+  font-family: 'Torus' ,'Varela Round' ,sans-serif;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,22 @@
 <template>
-  <News />
+  <div class="grid grid-cols-2 w-320">
+    <div class="ml-14 -mr-8 my-8 flex flex-col space-y-4">
+      <img class="rounded-xl" src="~/assets/miku.png">
+      <!-- Placeholder -->
+      <div class="mx-auto">
+          <svg width="110" height="6" viewBox="0 0 110 6" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path d="M0 3C0 1.34315 1.34315 0 3 0H27C28.6569 0 30 1.34315 30 3C30 4.65685 28.6569 6 27 6H3C1.34315 6 0 4.65685 0 3Z" fill="#66FFCC"/>
+          <rect opacity="0.5" x="40" y="2" width="30" height="2" rx="1" fill="#66FFCC"/>
+          <rect opacity="0.5" x="80" y="2" width="30" height="2" rx="1" fill="#66FFCC"/>
+        </svg>
+      </div>
+      <!--             -->
+      <div id="news" class="flex flex-col space-y-2">
+        <h1 class="text-xl text-white text-bold mb-2">News</h1>
+        <News />
+        <News />
+        <News />
+      </div>
+    </div>
+  </div>
 </template>
